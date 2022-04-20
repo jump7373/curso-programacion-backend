dccfcfd1088b1954b0b3b79cec728f3a74e8af6d
Juan Pablo Mirabile <78926404+jump7373@users.noreply.github.com>
2022-04-19 22:06:39 -0300

    Delete clase-5 directory

--- clase-5/02_nativos.js
+++
@@ -1,9 +0,0 @@
-//OS
-
-const os = require("os")
-
-// console.log(os.cpus())
-// console.log(os.arch())
-// console.log(os.endianness())
-const colors = require("colors")
-console.log("Hello".bold)
